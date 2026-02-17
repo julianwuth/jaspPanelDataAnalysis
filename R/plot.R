0566192a4ffe4dce80308d15c152d95dbaf7eb84
Jakub Jurak <kubojurak@gmail.com>
2026-02-17 15:16:24 +0100

    plot

--- R/plot.R
+++ R/plot.R
@@ -2,7 +2,7 @@
 #' @export
 .createPlmPlot <- function(jaspResults, dataset, options, ready){
 
-  plmPlot <- createJaspPlot(title = "Panel Plot",  width = 160, height = 320)
+  plmPlot <- createJaspPlot(title = "Line Plot",  width = 750, height = 300)
 
   plmPlot$dependOn(c("time", "id", "dependent", "plot"))
 
@@ -18,19 +18,22 @@
 
 .plmFillPlotDescriptives <- function(plmPlot, dataset, options){
   dataset <- dataset
-  dataset[[options$time]] <- as.numeric(dataset[[options$time]])
+
+  dataset[[options$time]] <- as.numeric(as.character(dataset[[options$time]]))
+
   rangeTime <- range(dataset[[options$time]])
 
-  xBreaks <- jaspGraphs::getPrettyAxisBreaks(rangeTime)
+  xBreaks <- unique(c(jaspGraphs::getPrettyAxisBreaks(rangeTime), rangeTime))
 
   aes <- ggplot2::aes
 
   plot <- ggplot2::ggplot(dataset, aes(dataset[[options$time]], dataset[[options$dependent]], color = dataset[[options$id]]))+ggplot2::geom_line()
 
   plot <- plot +
-    jaspGraphs::scale_x_continuous(options$time, breaks = xBreaks) +
-    jaspGraphs::themeJaspRaw() +
-    jaspGraphs::geom_rangeframe(sides = 'bl')
+    jaspGraphs::scale_x_continuous(options$time, breaks = xBreaks, limits = rangeTime) +
+    jaspGraphs::themeJaspRaw(legend.position = 'right') +
+    jaspGraphs::geom_rangeframe(sides = 'bl')+
+    ggplot2::labs(x = options$time , y = options$dependent, color = options$id)
 
   plmPlot$plotObject <- plot
 
